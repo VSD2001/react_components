3720f39729d4c79d860c847058b8c83759601375
VSD2001 <varad422001deshpande@gmail.com>
2025-11-24 11:16:18 +0530

    pp

--- src/Components/Header1/Header.jsx
+++ src/Components/Header1/Header.jsx
@@ -1,44 +1,11 @@
-import React, { useState } from "react";
-import "./header.scss";
-import logo from "../../assets/logo.png";
-import { IoGrid } from "react-icons/io5";
-import { Link } from "react-router-dom";
-import PopupMenu from "../PopupMenu/PopupMenu";
+import React from 'react'
 
-const Header = () => {
-  const [openPopup, setOpenPopup] = useState(false);
-
-  const handleGridClick = () => {
-    setOpenPopup((prev) => !prev);
-  };
-
+const header = () => {
   return (
-    <>
-      <div className="header">
-        <div className="logo">
-          <Link to="/">
-            <img src={logo} alt="logo" />
-          </Link>
-        </div>
+    <div>
+      
+    </div>
+  )
+}
 
-        <div className="menu">
-          <a href="#">Home</a>
-          <a href="#">About</a>
-          <a href="#">Services</a>
-          <a href="#">Portfolio</a>
-          <a href="#">Contact</a>
-          <a href="#">Blog</a>
-        </div>
-
-        {/* Only one click handler */}
-        <div className="grid" onClick={handleGridClick}>
-          <IoGrid />
-        </div>
-      </div>
-
-      {openPopup && <PopupMenu onClose={() => setOpenPopup(false)} />}
-    </>
-  );
-};
-
-export default Header;
+export default header
