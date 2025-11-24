a71fd224dcb3913220a2ae017df43036e2024556
aishwarya-0702 <aishwaryabhandare0702@gmail.com>
2025-11-24 15:40:08 +0530

    pp

--- src/Components/Header2/Header.jsx
+++ src/Components/Header2/Header.jsx
@@ -1,11 +1,27 @@
-import React from 'react'
+import React from "react";
+import "./Header.scss";
+import { Link } from "react-router-dom";
+import { IoMenuSharp } from "react-icons/io5";
+import logo from "../../Assets/logo.png";
 
 const Header = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <>
+      <div className="headerparent parent">
+        <div className="headercontainer cont">
+          <div className="logo">
+            <img src={logo} alt="mylogo" />
+          </div>
+          <div className="menu">
+            
 
-export default Header
+          </div>
+          <div className="menuicon"></div>
+
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Header;
